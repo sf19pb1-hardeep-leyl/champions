45cbbc6af349f8b0901913a31d3b644e431b9bb6
hardeep-leyl <54153538+hardeep-leyl@users.noreply.github.com>
2019-08-22 14:59:26 -0400

    Create european.py

---
+++ european.py
@@ -0,0 +1,18 @@
+import sys
+
+instructions = "How many times has Liverpool won the Champions League? You get 5 attempts"
+
+print(instructions)
+i = 0
+while  i <= 4:
+    guess = input("Go ahead: ")
+    n = int(guess)
+    if n == 6:
+        print("Correct!")
+        for i in range(9):
+            print("You'll Never Walk Alone")
+        sys.exit(0)
+    print("Wrong")
+    i = i+1
+print("The correct answer is 6.")
+sys.exit(0)
