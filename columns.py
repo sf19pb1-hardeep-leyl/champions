24baa14e4c9332295048fe22f182bc46e5331637
hardeep-leyl <54153538+hardeep-leyl@users.noreply.github.com>
2019-08-27 14:13:51 -0400

    Create columns.py

---
+++ columns.py
@@ -0,0 +1,21 @@
+import sys
+rn = input("How many rows of boxes (e.g., 3)? ")
+rows = int(rn)
+cn = input("How many columns of boxes (e.g., 4)? ")
+columns = int(cn)
+rs = input("How many rows of spaces in each box (e.g., 1)? ")
+row_space = int(rs)
+cs = input("How many columns of spaces in each box (e.g., 3)? ")
+col_space = int(cs)
+int(col_space)
+
+for r in range(rows):
+    for c in range(columns):
+        print("+", end = "")
+        for cs in range(col_space):
+            print("-", end = "")
+            cs += 1
+        c += 1
+    r += 1
+    print()
+sys.exit(0)
