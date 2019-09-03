862332bc075e835c292d1d55a97d189d52d2c82c
hardeep-leyl <54153538+hardeep-leyl@users.noreply.github.com>
2019-09-03 15:04:22 -0400

    Update name_game.py

--- name_game.py
+++ name_game.py
@@ -9,13 +9,13 @@ import sys
 while True:
 
     try:
-        word = input("Please type a name: ")
+        name = input("Please type a name: ")
     except EOFError:
         sys.exit(0)
 
-    rest = word[1:]
-    nameGame = word + "," + word + ",bo-b" + rest + "\n"
+    rest = name[1:]
+    nameGame = name + "," + name + ",bo-b" + rest + "\n"
     nameGame = nameGame + "Banana-fana fo-f" + rest + "\n"
     nameGame = nameGame + "Fee-fi-mo-m" + rest + "\n"
-    nameGame = nameGame + word + "!!"
+    nameGame = nameGame + name + "!!"
     print(nameGame)
